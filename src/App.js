db97f0e9127d3f2e2f7559c9aa43329cdb9fffd0
Abhay <26abhaykumarsingh@gmail.com>
2023-07-18 15:42:41 +0530

    Add: Charts Section without content

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import "./App.css";
 import Sidebar from "./components/Sidebar";
 import Header from "./components/Header";
+import Charts from "./components/Charts";
 
 function App() {
   return (
     <div className="App">
       <Sidebar className="sidebar" />
-      <Header className="header" />
+      <div className="container">
+        <Header className="header" />
+        <Charts className="charts" />
+      </div>
     </div>
   );
 }
---
+++ src/components/Charts.js
@@ -0,0 +1,24 @@
+import React from "react";
+import "./Charts.css";
+
+function Charts() {
+  return (
+    <div>
+      <div className="charts">
+        <div className="child one"></div>
+        <div className="child two"></div>
+        <div className="child three"></div>
+        <div className="child four"></div>
+        <div className="child five"></div>
+        <div className="child six"></div>
+        <div className="child seven">
+          <div className="sevenOne"></div>
+          <div className="sevenTwo"></div>
+          <div className="sevenThree"></div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Charts;
